0a0cb2b3904e4861b8688972e64fad747a70d71b
uki-no <84130177+uki-no@users.noreply.github.com>
2022-12-13 16:58:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var SpeechRecognition=window.webkitSpeechRecognition
+var recognition= new SpeechRecognition()
+
+function start(){
+    document.getElementById("textbox").innerHTML=""
+    recognition.start();
+}
